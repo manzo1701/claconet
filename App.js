cd79bc814cb758996b46e217710d16235ac60808
unknown <lautaroordenes.epet20@gmail.com>
2022-06-14 18:32:30 -0300

    commit

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import UsuarioForm from './src/ui/UsuarioForm';
+import LoginForm from './src/ui/LoginForm';
+import Saludar from './src/ui/saludar';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+    
+     
+     <LoginForm/>
+    
+    
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    borderColor: '#f0f8ff',
+    color: '#f0ffff',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "red",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    borderColor: '#f0f8ff',
+    color: '#f0ffff',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'top',
+  }
 });
---
+++ src/ui/LoginForm.js
@@ -0,0 +1,19 @@
+
+import react from "react";
+import {TextInput, Button, Text, View} from "react-native"
+
+
+export default function LoginForm (){
+    return(
+ 
+        <View>
+            
+            <TextInput placeholder="Email"/><br/>
+            <TextInput placeholder="Contraseña"/><br/>
+          
+            <Button title="Aceptar" onPress={() => console.log("Enviar")}/><br/>
+            <Button style="" title="Cancelar" onPress={() => console.log("Cancelar")}/>
+            
+        </View>
+    )
+}
