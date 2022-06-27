ac03643f8ef749bf004b8257d0e411ae20cd3808
unknown <lautaroordenes.epet20@gmail.com>
2022-06-27 18:48:14 -0300

    commit realizado

--- src/ui/ClasesConsulta.js
+++ src/ui/ClasesConsulta.js
@@ -1,14 +1,24 @@
 import react from 'react'
-import{Text, View, Button, TextInput} from "react-native";
+import{Text, View, Button, TextInput, StyleSheet} from "react-native";
+import { ImageBackground } from 'react-native-web';
 
 export default function ClasesConsulta() {
     return (
-    <View >
-    <Text>hola</Text>
+    <View style={styles.contenedor}>
+    <Text style={styles.style}>hola</Text>
     
     
     </View>
     
     )
 }
+
+const styles = StyleSheet.create({
+    style:{
+    color: '#ffff',
+    fontSize: 30,
+    fontFamily: 'nunito',
+    backgroundcolor:'black',
+    }
+})
 
