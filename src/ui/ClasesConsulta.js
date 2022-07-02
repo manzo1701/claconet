068ca779be0d892a3447ad62a7a013810e7cc214
unknown <lautaroordenes.epet20@gmail.com>
2022-07-02 14:31:18 -0300

    commit realizado

--- src/ui/ClasesConsulta.js
+++ src/ui/ClasesConsulta.js
@@ -5,15 +5,15 @@ import { ImageBackground } from 'react-native-web';
 export default function ClasesConsulta() {
     return (
     <View style={styles.contenedor}>
-<Text style={styles.header}>Clase de consulta</Text>
+<Text style={styles.header}>Clases de Consulta</Text>
         <TouchableOpacity onPress={() => Alert.alert('texto tocado')}>    
-        <Text style={styles.button}>Nombre jefatura</Text>
+        <Text style={styles.button}>Nombre Jefatura</Text>
         </TouchableOpacity>
         
-        <Text style={styles.box}>Profesor: </Text>
-        <Text>Nombre profe </Text>
-        <Text>Horarios: </Text>
-        <Text>Dia-horario </Text>
+        <Text style={styles.text}>Profesor: </Text>
+        <Text style={stylestext}>Nombre profe </Text>
+        <Text style={styles.text}>Horarios: </Text>
+        <Text style={styles.text}>Dia-horario </Text>
     
     
     </View>
@@ -33,6 +33,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     color: '#ffff',
+   },
+   text:{
+    color: '#ffff',
+    fontSize: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
    },
     button:{
     color: '#ffff',
