9277522b0a1943dabccca69effd3891ec4487d3c
unknown <lautaroordenes.epet20@gmail.com>
2022-07-28 18:13:53 -0300

    commit realizado

--- src/ui/ClasesConsulta.js
+++ src/ui/ClasesConsulta.js
@@ -1,5 +1,6 @@
 import react from 'react'
 import{Alert, Text, View, Button, TextInput, StyleSheet, TouchableOpacity} from "react-native";
+import { block } from 'react-native-reanimated';
 import { ImageBackground } from 'react-native-web';
 
 export default function ClasesConsulta() { //funcion que se encarga de la plantilla de una materia
@@ -9,11 +10,15 @@ export default function ClasesConsulta() { //funcion que se encarga de la planti
         <TouchableOpacity onPress={() => Alert.alert('texto tocado')}> 
         <Text style={styles.button}>Nombre Jefatura</Text>
         </TouchableOpacity>
-
+<View style={styles.box}>
         <Text style={styles.text}>Profesor: </Text>
         <Text style={styles.text}>Nombre profe </Text>
+        </View>
+        <View style={styles.box2}>
         <Text style={styles.text}>Horarios: </Text>
         <Text style={styles.text}>Dia-horario </Text>
+</View>                                
+        
     
     
     </View>
@@ -23,7 +28,16 @@ export default function ClasesConsulta() { //funcion que se encarga de la planti
 
 const styles = StyleSheet.create({ //estilos de la plantilla
    box:{ //estilo que intenta generar bordes en el contenido
+    backgroundcolor:'#ffff',
     borderColor: '#ffff',
+    boxsizing:'content-box',
+    display: 'inline-block',
+    padding: 20,
+   },
+   box2:{ //estilo que intenta generar bordes en el contenido
+    backgroundcolor:'#ffff',
+    borderColor: '#ffff',
+    display: 'inline-block',
    },
    
     header:{ //estilo encargado del header, este se encuentra en clase de consulta
@@ -33,11 +47,13 @@ const styles = StyleSheet.create({ //estilos de la plantilla
     color: '#ffff',
    },
    text:{ //estilo encargado del texto osea profesr, nombre profe, horarios y dia horario
-    color: '#ffff',
-    fontSize: 10,
+    color: 'black',
+    backgroundColor:'#ffff',
+    fontSize: 20,
     alignItems: 'center',
     justifyContent: 'center',
    },
+   
     button:{ //estilo dedicado a los botones este se situa temporalmente en nombre jefatura
     color: '#ffff',
     fontSize: 30,
