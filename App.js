d3ffe9b6fd898ba8bd8f99966fc175d7358771fd
EzequielA2021 <83735859+EzequielA2021@users.noreply.github.com>
2022-08-17 09:37:16 -0300

    archivos js cambiados de carpeta

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import UsuarioForm from './src/ui/UsuarioForm';
-import LoginForm from './src/ui/LoginForm';
-import Saludar from './src/ui/saludar';
+import UsuarioForm from './UI/components/UsuarioForm';
+import LoginForm from './UI/components/LoginForm';
+import Saludar from './UI/components/Saludar';
 import 'react-native-gesture-handler';
-import ProfesorForm from './src/ui/ProfesorForm';
+import ProfesorForm from './UI/components/ProfesorForm';
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
       
     
       <ProfesorForm/><br/>
-     <LoginForm/>
+      <LoginForm/>
     
     
     </View>
