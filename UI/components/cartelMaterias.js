722e685638f1dbfbd3e28a146717c5ce21251693
Ezeee <personal.ezequiel2019@gmail.com>
2022-08-23 13:08:49 -0300

    Arreglos

--- UI/components/cartelMaterias.js
+++ UI/components/cartelMaterias.js
@@ -2,43 +2,24 @@ import React from "react";
 import{Text, View, Button, TextInput} from "react-native";
 import { StyleSheet } from "react-native-web";
 
-<<<<<<< HEAD:src/ui/cartelMaterias.js
 export default function Cartel(){
 
     return(
         <View>
         <Text style={estilo.contenedor}>Hola buenas tardes como anda?</Text>
-=======
-export default function Cartel () {
-
-    return (
-        <View style={estilo.contenedor}>
-            <Text>Hola buenas tardes</Text>
->>>>>>> ad3d00c57ec2937bfe011bc90ac4f6820ca805ea:UI/components/cartelMaterias.js
         </View>
     )
 
 }
 
 const estilo = StyleSheet.create({
-<<<<<<< HEAD:src/ui/cartelMaterias.js
     contenedor:{
         padding: 10,
         margin: 10,
         backgroundColor: 'white',
-=======
-    contenedor: {
-        padding: 10,
-        backgroundColor: 'grey',
-        margin: 10,
->>>>>>> ad3d00c57ec2937bfe011bc90ac4f6820ca805ea:UI/components/cartelMaterias.js
         borderRadius: 10,
     }
 
 
-<<<<<<< HEAD:src/ui/cartelMaterias.js
 })
 
-=======
-});
->>>>>>> ad3d00c57ec2937bfe011bc90ac4f6820ca805ea:UI/components/cartelMaterias.js
