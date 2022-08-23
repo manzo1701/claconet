317b64149bd50c57603a7dd520562ba40c30e25c
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-08-23 15:01:07 -0300

    App.js

--- App.js
+++ App.js
@@ -1,36 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import './src/ui/Components/UsuarioForm'
+import React from 'react'
+import AppNavigation from '@navigation/AppNavigation'
 
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-    
-    
-    </View>
-    
-  );
+function App(){
+  return <AppNavigation/>
 }
-//Hola mundo
 
-
-const styles = StyleSheet.create({
-  container: {
-    borderColor: '#f0f8ff',
-    color: '#f0ffff',
-    flex: 1,
-    backgroundColor: "grey",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  container2: {
-    borderColor: '#f0f8ff',
-    color: '#f0ffff',
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'top',
-  }
-});
-
+export default App
