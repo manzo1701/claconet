39b34f4d9fe348ab2bee02795b7b351f7d2b18cb
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-08-25 13:36:32 -0300

    Estilos para las pantallas

--- app/styles/General.js
+++ app/styles/General.js
@@ -24,6 +24,37 @@ const loginStyles = StyleSheet.create({
         paddingTop: 50,
         alignItems: 'center',
     },
+
+    btnMain: {
+        width: 280,
+        marginTop:40,
+        marginBottom: 20,
+        backgroundColor: Colors.BLUE,
+        borderRadius: 60
+    },
+
+    btnTransparent: {
+        backgroundColor: 'rgba(52, 52, 52, 0)',
+        borderColor: Colors.BLUE,
+        width: 280,
+        borderWidth: 2,
+        marginBottom: 20,
+        borderRadius: 60
+    },
+
+    btntxt: {
+        textAlign: 'center',
+        fontSize: 17,
+        color: Colors.WHITE,
+        paddingVertical: 15,
+        fontFamily: 'nunito.semibold',
+    },
+
+    txtTransparent: {
+        color: Colors.LIGHTPRIMARYCOLOR,
+        fontSize: 14,
+        fontFamily: 'nunito.semibold',
+    }
 })
 
-export {imageBackgroundStyle}
+export { loginStyles, splashStyles }
