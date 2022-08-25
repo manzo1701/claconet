bff2bd4a9894cc38cf6cc298f46b8354fb562f6e
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-08-25 10:08:30 -0300

    Dos funciones mas

--- app/storage/UsuarioAsyncStorage.js
+++ app/storage/UsuarioAsyncStorage.js
@@ -2,6 +2,7 @@ import AsyncStorage from "@react-native-community/async-storage" //Dependencia
 
 const USUARIO_KEY = '@usuario:key'
 
+//Funciones
 
 async function saveUsuario(usuario){ //Recibo usuario que esta convertido en JSON
     try{
@@ -13,3 +14,25 @@ async function saveUsuario(usuario){ //Recibo usuario que esta convertido en JSO
     }
 }
 
+async function getUsuario(){
+    try {
+        const item = await AsyncStorage.getItem(USUARIO_KEY) //Llamo a AsyncStorage y con la funcion getItem le mando el key 
+        return JSON.parse(item) //Con parse transformo el string a JSON
+    } catch (error) {
+        console.log("Error al recuperar:" + error.message)
+        return null
+    }
+}
+
+async function deleteUsuario(){ //Usuario elimine su sesion
+    try {
+        await AsyncStorage.removeItem(USUARIO_KEY) //Envio de KEY para que lo puede remover
+        const item = await AsyncStorage.getItem(USUARIO_KEY) //Llamo a getItem 
+        return (item == null?"usuario removido":"usuario no removido") //nul para ver si se removio al usuario 
+    } catch (error) {
+        console.log("Error al eliminar" + error.message)
+        return "Error de sintaxis"
+    }
+}
+
+export {saveUsuario, getUsuario, deleteUsuario} 
