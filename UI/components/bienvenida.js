e2d122c7dfebd31d05720e38ff82c2a78f667eb1
EzequielA2021 <83735859+EzequielA2021@users.noreply.github.com>
2022-09-13 15:46:29 -0300

    mas arreglos

--- UI/components/bienvenida.js
+++ UI/components/bienvenida.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import { StyleSheet } from "react-native";
+import { View } from "react-native-animatable";
 export function Bienvenida(props){
 
 
     return(
     
-    <h1>Bienvenido { props.profesor }</h1>
+    <View style={styles.estilo}>    
+        <h1>Bienvenido { props.profesor }</h1>
+    </View>
     )
 }
 
+const styles = StyleSheet.create({
+
+    estilo:{
+        fontSize: 14,
+        fontFamily: 'Arial'
+    }
+})
--- UI/components/cartelMaterias.js
+++ UI/components/cartelMaterias.js
@@ -1,7 +1,5 @@
 import React from "react";
-import{Text, View, Button, TextInput} from "react-native";
-import { StyleSheet } from "react-native-web";
-
+import{Text, View, Button, StyleSheet} from "react-native";
 export default function Cartel(props){
     const { materia, horario1, horario2 } = props       //Se lee lo que contienen las variables en el props
     return(
@@ -28,6 +26,5 @@ const estilo = StyleSheet.create({      //style para armar el cartel de materia
         justifyContent: 'center',
         fontSize: 24,
     }
-
 
 })
