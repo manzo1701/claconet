bc88e754c0ecd282a4b74797645450fc92b364ef
EzequielA2021 <83735859+EzequielA2021@users.noreply.github.com>
2022-10-05 15:43:30 -0300

    conexion con base de datos

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZvNPgbwzay7-xsENI8HTCNJR6ALMho-0",
+    authDomain: "claconet-35b91.firebaseapp.com",
+    projectId: "claconet-35b91",
+    storageBucket: "claconet-35b91.appspot.com",
+    messagingSenderId: "540813190742",
+    appId: "1:540813190742:web:6d5b4aa9aa380494363df7",
+    measurementId: "G-MXZE4X2423"
+  };
