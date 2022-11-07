9d29c14fe5797fa3dec9ea9f09fe463d1b9e7b18
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-11-06 21:19:15 -0300

    Mensajes de errores js

--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,5 +1,7 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
 import {auth} from './firebase.js'
+import {showMessage} from './showMessage.js'
+
 const signupForm = document.querySelector('#signup-form') //Identifico el elemento "signup-form" y lo guardo en una constante
 
 signupForm.addEventListener('submit', async (event) => { //De signupForm uso el evento "addEventListener", que escucha cuando se envia el formulario
@@ -14,9 +16,23 @@ signupForm.addEventListener('submit', async (event) => { //De signupForm uso el 
         const userCredentials = await createUserWithEmailAndPassword(auth, email, password) //Promesa await
         console.log(userCredentials)
 
+        //Cerrar el modal de registro
         const signupModal = document.querySelector('#signupModal')
-        bootstrap.Modal.getInstance(signupModal)
+        const modal = bootstrap.Modal.getInstance(signupModal)
+        modal.hide()
+
+        showMessage("Welcome " + userCredentials.user.email)
+
+
     } catch (error) {
-        console.log(error)
-    }
+        if (error.code === 'auth/email-already-in-use') {
+            showMessage("Email already in use", "error")
+        } else if (error.code === 'auth/invalid-email') {
+            showMessage("Invalid email", "error")
+        } else if (error.code === 'auth/weak-password') {
+            showMessage("Weak password", "error")
+        } else if (error.code) {
+            showMessage("Something went wrong", "error")
+        }  
+    }   
 })
