129c06e6029947c0a5f8cb556549ad48123b8577
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-11-06 23:47:43 -0300

    Main actualizado

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
+import { auth } from "./app/firebase.js"
+import {loginCheck} from './app/loginCheck.js'
 import './app/signupForm.js'
-// import '/reacts/claconet/claconet/src/app/signupForm.js'
+import './app/logout.js'
 
+onAuthStateChanged(auth, async (user) =>{
+    loginCheck(user)
+    // if (user){
+    // }else{
+    //     loginCheck(user)
+    // }
+})
