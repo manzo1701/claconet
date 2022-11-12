f7602aa619c2196885fec153a2519eefe0f4cf75
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-11-12 17:15:31 -0300

    Auntenticacion con google lista

--- src/app/googleLogin.js
+++ src/app/googleLogin.js
@@ -1,11 +1,25 @@
+import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
+import { auth } from './firebase.js'
+import { showMessage } from './showMessage.js'
+
 const googleButton = document.querySelector('#googleLogin') //Identifico el boton de Login
-import { googleAuthProvider, signInwithPopup } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
-import { Auth } from './firebase,js'
 
-googleButton.addEventListener('click', () => {
+googleButton.addEventListener('click', async () => {
+    
+    const provider = new GoogleAuthProvider() //Instancia de googleAuth, la clase devuelve un objeto
 
-    const provider = new googleAuthProvider() //Instancia de googleAuth, la clase devuelve un objeto
+    try {
+     const credentials = await signInWithPopup(auth, provider) //Lanzar ventana de autenticacion
+     console.log(credentials)
 
-    signInwithPopup(Auth, provider) //Lanzar ventana de autenticacion
+     const modal = bootstrap.Modal.getInstance(document.querySelector('#signinModal'))
+     modal.hide()
+     showMessage('Welcome '+ credentials.user.displayName, 'success')
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+
 
 })
