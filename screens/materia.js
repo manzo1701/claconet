b6c13f22519515469eae12a49a8db5b7f2b2af98
EzequielA2021 <83735859+EzequielA2021@users.noreply.github.com>
2022-11-12 17:28:32 -0300

    lectura de datos para crear clase

--- screens/materia.js
+++ screens/materia.js
@@ -1,15 +1,35 @@
 import { Text, View, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Boton from '../UI/components/Boton';
+import { Button } from 'react-native-web';
 
 const Materia = ({ navigation }) => {
+
+    const [ state, setState ] = useState({
+
+        nom: "",
+        desde: "",
+        hasta: "",
+    })
+
+    const handleChangeText = (nom, value) => {
+        setState({ ...state, [nom]: value})
+    }
 
     return(
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
 
-            <TextInput style={estilo.input} placeholder='Nombre de la materia'/>
-            <TextInput style={estilo.input} placeholder='Desde'/>
-            <TextInput style={estilo.input} placeholder='Hasta'/>
+            <TextInput style={estilo.input} placeholder='Nombre de la materia'
+                onChangeText={(value) => handleChangeText('nom', value)}
+            />
+            <TextInput style={estilo.input} placeholder='Desde'
+                onChangeText={(value) => handleChangeText('desde', value)}
+            />
+            <TextInput style={estilo.input} placeholder='Hasta'
+                onChangeText={(value) => handleChangeText('hasta', value)}
+            />
+
+            <Button title='Subir' onPress={() => console.log(state)}/>
             <Boton
                 text= 'volver'
                 onPress = {()=> navigation.navigate("Home")}
