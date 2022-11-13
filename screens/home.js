05cd1a1c9b06b337055e4bea739822e2874f1733
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-11-13 20:44:07 -0300

    Pantalla home de ejemplo

---
+++ screens/home.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import { View, Text } from 'react-native'
+// import Boton from '../components/button'
+
+// const Home = ({navigation}) => {
+//   return (
+//     <View>
+
+//       <Boton
+//         text = "Ir al perfil"
+//         onPress = {() => {
+//             navigation.navigate('profile')
+//         }}
+//       />
+        
+//     </View>
+//   )
+// }
+
+// export default Home
