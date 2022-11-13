06871a3fe99fa2c9126522086356e9a6bae7c338
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-11-13 20:47:44 -0300

    Navegacion

---
+++ navigation/mainStack.js
@@ -0,0 +1,36 @@
+// import React from 'react'
+// import {createNativeStackNavigator} from '@react-navigation/native-stack'
+// import {NavigationContainer} from '@react-navigation/native'
+// import Home from '../screens/home'
+// import Profile from '../screens/profile'
+
+// const Stack = createNativeStackNavigator()
+
+
+// const MainStack = () => {
+//     return (
+//         <div>
+
+//     <NavigationContainer> 
+//         <Stack.Navigator
+//         screenOptions= {{
+//             headerShown: false,
+//         }}
+//         > 
+//             <Stack.Screen 
+//             name='Home'
+//             component= {Home}
+//             />
+
+//             <Stack.Screen 
+//             name='Profile'
+//             component= {Profile}
+//             />
+//         </Stack.Navigator>
+//     </NavigationContainer>
+//         </div>
+    
+//     )
+// }
+
+// export default MainStack
