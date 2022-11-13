65de4d678c967b047eea5ea5d5ce994e9b9b4c7c
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-11-13 20:45:00 -0300

    Pantallas de ejemplo

---
+++ screens/profile.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+// import { View, Text } from 'react-native'
+
+// const Profile = ({navigation}) => {
+//   return (
+//     <View>
+//       <Text>Mi perfil</Text>
+//     </View>
+//   )
+// }
+
+// export default Profile
