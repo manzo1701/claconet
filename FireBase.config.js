fcdd5acfa7517e5127dcb13ccdaad964e8932de1
santiagorojas-cod <santiagorojas.epet20@gmail.com>
2022-11-13 20:27:50 -0300

    Configuracion de FireBase desuso

---
+++ FireBase.config.js
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAZvNPgbwzay7-xsENI8HTCNJR6ALMho-0",
+//   authDomain: "claconet-35b91.firebaseapp.com",
+//   projectId: "claconet-35b91",
+//   storageBucket: "claconet-35b91.appspot.com",
+//   messagingSenderId: "540813190742",
+//   appId: "1:540813190742:web:6d5b4aa9aa380494363df7",
+//   measurementId: "G-MXZE4X2423"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
